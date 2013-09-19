86cd6f3fcef9ed8608eabf070089c7299978c473
Alexander Dovzhikov <alexander.dovzhikov@excelian.com>
2013-09-20 09:59:25 +1000

    Q27 completed

--- q27.R
+++ q27.R
@@ -3,5 +3,17 @@ source("partition.R")
 
 monthly <- loadMonthly()
 sp <- diff(log(monthly[,"S.PCOMP"]))
+# runs
 psp <- partition.returns(sp)
-hist(psp)
+barplot(table(psp), col="#818AFC", space=.8)
+title(main="Run length", xlab="Run length", ylab="Frequency")
+
+# signed runs for S&P
+spsp <- psp * rep(c(1,-1), length.out=length(psp))
+upmonth.prob <- 100 * sum(spsp[spsp > 0]) / sum(psp)
+downmonth.prob <- 100 * sum(-spsp[spsp < 0]) / sum(psp)
+longest.run <- max(psp)
+
+cat(sprintf("a) probability of an up month: %.2f%%\n", upmonth.prob))
+cat(sprintf("b) probability of a down month: %.2f%%\n", downmonth.prob))
+cat(sprintf("c) longest run: %d\n", longest.run))
