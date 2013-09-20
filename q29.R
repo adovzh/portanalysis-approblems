578ce9a7c8588ae5cd5364f8280c27e132154b78
Alexander Dovzhikov <alexander.dovzhikov@excelian.com>
2013-09-20 12:17:03 +1000

    Added some test diagnostics

--- q29.R
+++ q29.R
@@ -11,3 +11,6 @@ title(main="All Ordinaries Run length", xlab="Run length", ylab="Frequency")
 cat(sprintf("a) probability of an up month: %.2f%%\n", rt$upmonth.prob))
 cat(sprintf("b) probability of a down month: %.2f%%\n", rt$downmonth.prob))
 cat(sprintf("c) longest run: %d\n", rt$longest.run))
+
+# a <- rt$runs
+# lapply(mapply(function(a,len) sp[seq(to=a, length.out=len)], cumsum(a), a), sign)
