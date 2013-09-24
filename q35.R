7ca4635c36676881b63b1716f4b82aa5a5a5b211
Alexander Dovzhikov <alexander.dovzhikov@excelian.com>
2013-09-24 10:22:21 +1000

    Annualising statistics

--- q35.R
+++ q35.R
@@ -6,9 +6,11 @@ monthly <- toTimeSeries(loadMonthly())
 usb <- monthly[,"USBD10Y"]
 bond.returns <- timeSeries(data=bond.hpr.monthly(usb[-length(usb)], usb[-1]),
                            charvec=rownames(usb)[-1])
-names(bond.returns) <- names(usb)
+names(bond.returns) <- "Adj.Bond Returns"
 
 # print statistics
 library(fBasics)
-ret.stats <- cbind(basicStats(bond.returns), basicStats(usb))[c("Mean", "Stdev"), ]
+stat.names <- c("Mean", "Stdev")
+bond.stats <-basicStats(bond.returns)[stat.names, ] * c(12, sqrt(12))
+ret.stats <- cbind(bond.stats, basicStats(usb))[stat.names, ]
 print(ret.stats)
--- q36.R
+++ q36.R
@@ -10,5 +10,6 @@ bond.returns <- timeSeries(data=bond.hpr.monthly(usb[-length(usb)], usb[-1]),
 names(bond.returns) <- names(usb)
 
 sp.returns <- na.omit(fapply(monthly[,"S.PCOMP.RI."], FUN=returns))
-ret.stats <- cbind(basicStats(bond.returns), basicStats(sp.returns))
-print(ret.stats[c("Mean", "Stdev"), ])
+ret.stats <- cbind(basicStats(bond.returns), basicStats(sp.returns))[c("Mean", "Stdev"), ]
+rs.annualised <- ret.stats * c(12, sqrt(12))
+print(rs.annualised)
