56f19e5e7cf72b06feb942ab3346d99acca987be
Alexander Dovzhikov <alexander.dovzhikov@excelian.com>
2013-09-24 11:17:23 +1000

    Q38

--- q37-38.R
+++ q37-38.R
@@ -21,3 +21,26 @@ points(x, dnorm(x, mean=mean(excess.returns)*12, sd=sd(excess.returns)*sqrt(12))
        col="darkblue", type='l', lwd=2)
 legend("topright", c("USBD10Y", "S&P Accum", "Excess"), 
        col=c("darkgreen", "darkred", "darkblue"), lwd=2)
+
+bond.mean <- mean(bond.returns) * 12
+bond.var <- var(bond.returns) * 12
+bond.sd <- sqrt(bond.var)
+
+sp.mean <- mean(sp.returns) * 12
+sp.var <- var(sp.returns) * 12
+sp.sd <- sqrt(bond.var)
+
+excess.mean <- mean(excess.returns) * 12
+excess.var <- var(excess.returns) * 12
+excess.sd <- sqrt(excess.var)
+
+bond.sp.cov <- cov(bond.returns, sp.returns) * 12
+
+report <- data.frame(c(excess.mean, excess.var, excess.sd),
+                     c(sp.mean - bond.mean, 
+                       sp.var+bond.var-2*bond.sp.cov,
+                       sqrt(sp.var+bond.var-2*bond.sp.cov)))
+rownames(report) <- c("Mean", "Variance", "Stdev")
+colnames(report) <- c("Sample", "Formula")
+
+print(report)
