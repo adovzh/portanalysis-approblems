6a2a15537d0836b07450d6a67c75738f3f4039a2
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2013-09-26 08:25:16 +1000

    Barplot in progress

--- q43.R
+++ q43.R
@@ -7,5 +7,9 @@ ind <- factor(rep(1:4, each=length(sp.returns)/4), labels="Column")
 ds <- data.frame(series(sp.returns), ind)
 print(m <- anova(lm(S.PCOMP.RI. ~ ind, data=ds)))
 
+pval <- m[1,5]
 cat(sprintf("\nWe %s reject the null hypothesis that the groups have the same mean.\n",
-            ifelse(m[1,5] < .05, "can", "can't")))
+            ifelse(pval < .05, "can", "can't")))
+group.means <- aggregate(series(sp.returns), by=list(ind), FUN=mean)
+rownames(group.means) <- as.character(group.means[,1])
+group.means <- group.means[2]
