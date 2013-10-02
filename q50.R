ee1363640f19a1e0f57603959e8f573f4fbc7e41
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2013-10-02 19:55:17 +1000

    Q50

--- q50.R
+++ q50.R
@@ -6,18 +6,39 @@ weekly <- toTimeSeries(loadWeekly())[,1:15]
 weekly <- weekly[, colnames(weekly) != "WSF"]
 weekly.returns <- na.omit(fapply(weekly, FUN=returns))
 means <- colMeans(weekly.returns) * 52
+risks <- colStdevs(weekly.returns) * sqrt(52)
 sigma <- cov(weekly.returns) * 52
 
-gmvp <- findGMVP(sigma)
-max.ret.mvp <- findMVP(sigma=cov(weekly.returns), 
-                       ret=means,
-                       target.return=max(means))
+w1 <- findGMVP(sigma)
+w2 <- findMVP(sigma, 
+              ret=means,
+              target.return=max(means))
 
-conv.comb.f <- function(w1, w2) {
-  ms <- means %*% cbind(w1, w2)
-  function(alpha) crossprod(c(alpha, 1 - alpha), c()
+# conv.comb.f <- function(w1, w2) {
+#   ms <- means %*% cbind(w1, w2)
+#   function(alpha) crossprod(c(alpha, 1 - alpha), c()
+# }
+
+z <- function(alpha) {
+  cbind(w1, w2) %*% c(alpha, 1 - alpha)
 }
 
-aa <- function(alpha) {
-  z <- c(alpha, 1 - alpha) %*% rbind(gmvp, max.ret.mvp)
+mv.coords <- function(alpha) {
+  risk <- t(z(alpha)) %*% sigma %*% z(alpha)
+  ret <- means %*% z(alpha)
+  c(risk, ret)
 }
+
+alphas <- seq(-1, 1, by=.1)
+mv.points <- sapply(alphas, mv.coords)
+
+eff.risks <- mv.points[1,]
+eff.returns <- mv.points[2,]
+xrng <- c(eff.risks)
+plot(eff.risks, eff.returns, pch=16, axes=FALSE)
+axis(1, at=pretty(xrng), 
+     labels=sprintf("%.2f%%", pretty(xrng)*100))
+axis(2, at=pretty(eff.returns), 
+     labels=sprintf("%.2f%%", pretty(eff.returns)*100))
+box()
+grid()
