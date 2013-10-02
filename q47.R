252cf1d07fa1b0bb3804aee40eddbf8135098ef7
Alexander Dovzhikov <alexander.dovzhikov@excelian.com>
2013-10-02 10:44:26 +1000

    Q47

--- q47.R
+++ q47.R
@@ -5,7 +5,7 @@ source("common.R")
 weekly <- toTimeSeries(loadWeekly())[,1:15]
 weekly.returns <- na.omit(fapply(weekly, FUN=returns))
 
-# stocks <- sample(1:5, 3)
+# stocks <- sample(1:15, 3)
 stocks <- c(5,2,3)
 data <- as.timeSeries(weekly[,stocks])
 
@@ -13,9 +13,10 @@ absMinRiskWeights <- function(tS) {
   om <- cov(tS)
   ominv <- solve(om)
   l <- rep(1, ncol(tS))
-  return(ominv %*% l %*% (t(l) %*% ominv %*% l))
+  return(ominv %*% l %*% solve(t(l) %*% ominv %*% l))
 }
 
+cat("Weights using formula:\n")
 print(absMinRiskWeights(data))
 
 
@@ -23,3 +24,5 @@ s <- solve.QP(cov(data),
               matrix(rep(0, ncol(data))), 
               matrix(rep(1, ncol(data))), 
               1, 1)
+cat("\nWeights solving quadratic programming problem:\n")
+print(matrix(s$solution, dimnames=list(colnames(data))))
